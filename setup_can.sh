06fa2a63da50b0d06697f8762ebab75abd8a10f0
1in-oos <761456431@qqc.com>
2024-07-09 16:01:28 +0800

    修改log记录形式

--- setup_can.sh
+++ setup_can.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Enter interface (can0/vcan0):"
+# 提示用户输入接口名称，如果没有输入，则使用默认的 can0
+echo "Enter interface (default: can0):"
 read interface
 
+# 如果接口名称为空，设置为默认的 can0
+if [ -z "$interface" ]; then
+    interface="can0"
+fi
+
+# 判断接口名称，并执行相应的配置
 if [ "$interface" == "can0" ]; then
     echo "Enter bitrate (default: 500000):"
     read bitrate
@@ -19,9 +26,8 @@ elif [ "$interface" == "vcan0" ]; then
     sudo modprobe can
     sudo modprobe can_raw
     sudo modprobe vcan
-    sudo ip link add dev can0 type vcan
-    sudo ip link set up can0
+    sudo ip link add dev vcan0 type vcan
+    sudo ip link set up vcan0
 else
     echo "Invalid interface. Please enter either can0 or vcan0."
 fi
-
